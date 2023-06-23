7e129ff16c3519917959db2f7f65b8b5eff2c9d9
varshaver <varshaveraleksey@gmail.com>
2023-06-23 21:50:21 +0300

    Initial Commit

---
+++ Personal Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personal Quiz
+//
+//  Created by 1234 on 23.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
